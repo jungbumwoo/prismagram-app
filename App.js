ea1a8dc2931c93ca236beb84b907d03cc220030e
jungbum <jungbumwoo@naver.com>
2020-05-13 07:40:41 +0900

    0513

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import { Apploading, Font } from "expo";
+import { Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+  const [ loaded, setLoaded ] = useState(false);
+  const preload = async() => {
+    try {
+      await Font.loadAsync({
+        ...Ionicons.font
+      });
+      setLoaded(true);
+    } catch(e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    preload()
+  }, []);
+  return loaded ? <View>
+    <Text>asdfasdfdsafdfdfffffffffffffffffffff</Text>  
+  </View> : (
+  <Apploading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
