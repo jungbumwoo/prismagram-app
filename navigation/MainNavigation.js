bfd9a0509572cc0f0409280890766dd6c32edde3
jungbum <jungbumwoo@naver.com>
2020-05-24 17:55:56 +0900

    0524

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,8 +1,12 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import TabNavigation from "./TabNavigation";
+import PhotoNavigation from "./PhotoNavigation";
+import MessageNavigation from "./MessageNavigation";
 
 const MainNavigation = createStackNavigator({
-    TabNavigation
+    TabNavigation,
+    PhotoNavigation,
+    MessageNavigation
 });
 
 export default createAppContainer(MainNavigation);
